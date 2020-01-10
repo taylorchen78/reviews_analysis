bb2b38661933d6204e5efb047f106e6e74d63f8c
Taylor Chen <s68175@gmail.com>
2020-01-11 00:23:48 +0800

    Add reviews length average

--- reviews_analysis.py
+++ reviews_analysis.py
@@ -1,10 +1,14 @@
 data = []
 count = 0
+reviews_len = 0
+
 with open('reviews.txt', 'r') as file:
 	for line in file:
 		data.append(line)
-		count = count + 1
+		reviews_len += len(line)
+		count += 1
 		if count % 1000 == 0:
 			print(len(data))
 
-print('Read done')
+print('\nRead done')
+print('\nReviews length averge: ', reviews_len/count)
