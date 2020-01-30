105ee00fe4460b44384dc654611e15b939c5eea8
Taylor Chen <s68175@gmail.com>
2020-01-30 23:37:30 +0800

    add word count function

--- reviews_analysis.py
+++ reviews_analysis.py
@@ -12,3 +12,25 @@ with open('reviews.txt', 'r') as file:
 
 print('\nRead done')
 print('\nReviews length averge: ', reviews_len/count)
+
+word_count = {}
+for d in data:
+	words = d.split()
+	for word in words:
+		if word in word_count:
+			word_count[word] += 1
+		else:
+			word_count[word] = 1
+
+for word in word_count:
+	if word_count[word] > 100:
+		print(word, word_count[word])
+
+print(len(word_count))
+
+
+user_word = input('Please enter word: ')
+if user_word in word_count:
+	print(user_word, 'shows in ', word_count[user_word], ' times')
+else:
+	print(user_word, 'not in list')
